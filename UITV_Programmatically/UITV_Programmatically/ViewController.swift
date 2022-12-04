6b23ab84c7d96e5214a9b412a038e5b53442d5d2
Oriol Angelet <uriaeri@gmail.com>
2022-12-04 18:14:58 +0100

    first commit

---
+++ UITV_Programmatically/UITV_Programmatically/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UITV_Programmatically
+//
+//  Created by Uri on 4/12/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
